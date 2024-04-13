97e234633adfaca073159024f4260f2532a25ba6
Duke242 <innominate3301@proton.me>
2024-04-12 20:55:08 -0400

    build fix

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import { getSEOTags } from "@/libs/seo"
 import ClientLayout from "@/components/LayoutClient"
 import config from "@/config"
 import "./globals.css"
+import { Suspense } from "react"
 
 const font = Nunito({ subsets: ["latin"], weight: "400" })
 
@@ -28,7 +29,9 @@ export default function RootLayout({ children }) {
       )}
       <body>
         {/* ClientLayout contains all the client wrappers (Crisp chat support, toast messages, tooltips, etc.) */}
-        <ClientLayout>{children}</ClientLayout>
+        <Suspense>
+          <ClientLayout>{children}</ClientLayout>
+        </Suspense>
       </body>
     </html>
   )
