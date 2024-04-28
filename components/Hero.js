d92bdc1274e2c6fa7d9c718c331728bfc56b01d4
Duke242 <innominate3301@proton.me>
2024-04-28 13:23:09 -0400

    copy change

--- components/Hero.js
+++ components/Hero.js
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <section className="max-w-7xl mx-auto bg-base-100 flex flex-col lg:flex-col items-center justify-center gap-16 lg:gap-20 px-8 py-8 lg:py-20">
       <div className="text-center">
-        <h1 className="text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-300 to-blue-500 mb-4">
+        <h1 className="text-6xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-300 bg-[#87D8F5] mb-4">
           AI To Help You Write Better
         </h1>
         <p className="text-2xl text-gray-500">
@@ -13,7 +13,7 @@ const Hero = () => {
         </p>
       </div>
       <div className="w-1/2 h-64 relative">
-        <div className="absolute inset-0 bg-blue-500 filter blur-3xl opacity-50 rounded-lg"></div>
+        <div className="absolute inset-0 bg-[#87D8F5] filter blur-3xl opacity-50 rounded-lg"></div>
         <iframe
           className="w-full h-full relative z-10 rounded-lg"
           src="https://www.youtube.com/embed/qaDL4go6UvY?rel=0&autoplay=1&mute=1"
